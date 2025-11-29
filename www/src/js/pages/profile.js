5e8fc0ae4579b5c5e619edee8dfbf582cccd74e3
Aron <your-email@example.com>
2025-11-29 15:31:04 +0800

    giia

--- www/src/js/pages/profile.js
+++ www/src/js/pages/profile.js
@@ -8,8 +8,14 @@ async function loadUserName() {
         const name = data.user.user_metadata?.display_name || "User";
 
         const usernameElement = document.getElementById("username");
-        if (usernameElement) {
+          const nameElement = document.getElementById("name");
+            const namepost = document.getElementById("Name_post");
+          
+        if (usernameElement ||nameElement || namepost ) {
             usernameElement.textContent = name;
+            nameElement.textContent = name;
+            namepost.textContent = name + "'s Posts";
+
         }
     } else {
         console.log("User not logged in");
